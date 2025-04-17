be7c3a09c56e82cb617f28db0e1c242a8c99211f
AbdellatifMajd7 <AbdellatifMajd10@gmail.com>
2025-04-18 00:36:05 +0100

    commit changes

--- server/controllers/auth/user.controler.js
+++ server/controllers/auth/user.controler.js
@@ -1,37 +1,36 @@
-const User = require('../../models/User'); 
-const bcrypt = require('bcryptjs');
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
-    try{
-        const {fullName, email, password, confirmPassword} = req.body; 
-        if(password !== confirmPassword){
-            return res.send("passwords do not match!");
-        }
-        const hashPassword = await bcrypt.hash(password, 10);
+  try {
+    const { fullName, email, password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+      return res.send("passwords do not match!");
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
 
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.json({
-                message: 'User already exist, please try again.',
-                success: false
-            })
-        }
-
-        const newUser = new User({fullName, email, password:hashPassword}); 
-        await newUser.save();
-
-        return res.status(201).json({
-            message: "User created successfully.",
-            success: true
-        })
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({
+        message: "User already exist, please try again.",
+        success: false,
+      });
     }
 
-    catch(error){
-        return res.status(401).json({
-            error: true, 
-            message: "an error occured", error 
-        })
-    }
-}
+    const newUser = new User({ fullName, email, password: hashPassword });
+    await newUser.save();
 
-module.exports = {register}
+    return res.status(201).json({
+      message: "User created successfully.",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(401).json({
+      error: true,
+      message: "an error occured",
+      error,
+    });
+  }
+};
+
+module.exports = { register };
