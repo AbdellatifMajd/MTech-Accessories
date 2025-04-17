4538355791f9a06b669041d5065677d401a1849c
AbdellatifMajd7 <AbdellatifMajd10@gmail.com>
2025-04-18 00:01:41 +0100

    login API

--- server/routes/auth/routers.js
+++ server/routes/auth/routers.js
@@ -1,7 +1,8 @@
-const {register} = require('../../controllers/auth/user.controler')
+const {register, login} = require('../../controllers/auth/user.controler')
 const express = require('express'); 
 const router = express.Router(); 
 
 router.post("/register", register);
+router.post("/login", login)
 
 module.exports = router
